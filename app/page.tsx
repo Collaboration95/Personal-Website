c42b69b1893a8067db8d8aad3cc5c6356d04e8f4
Guruprasath <guruprasath3200@gmail.com>
2024-09-28 13:30:01 +0800

    Added About me

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import About from "@/components/about";
 import SectionDivider from "@/components/section-divider";
 import Intro from "@/components/intro";
 export default function Home() {
   return <main className="flex flex-col items-center px-4">
      <Intro/>
      <SectionDivider/>
+     <About/>
   </main>;
 }
---
+++ components/about.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <section className='mb-28 max-w-[45rem] text-center  leading-8 sm:mb-40'>
+        <h2 className='text-3xl font-medium capitalize mb-8'>About me</h2>
+        <p >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+    </section>
+  )
+}
