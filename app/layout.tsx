02c9fba911977045d340da877caff094ebf23695
Guruprasath <guruprasath3200@gmail.com>
2024-09-30 21:38:26 +0800

    Working Website

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
           <Header />
           {children}
         </ActiveSectionContextProvider>
-        <ThemeSwitch />
+        {/* <ThemeSwitch /> */}
         </ThemeContextProvider>
       </body>
     </html>
