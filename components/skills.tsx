c2d89f6b505535c1b99e31f1277fa5d2d619bbd8
Guruprasath <guruprasath3200@gmail.com>
2024-10-02 15:31:48 +0800

    Fixed lightmode selectivity for hover

--- components/skills.tsx
+++ components/skills.tsx
@@ -5,6 +5,8 @@ import SectionHeading from "./section-heading";
 import { skillsData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
 import { motion } from "framer-motion";
+import clsx from "clsx";
+import { useTheme } from "@/context/theme-context";
 
 const fadeInAnimationVariants = {
   initial: {
@@ -21,6 +23,7 @@ const fadeInAnimationVariants = {
 };
 
 export default function Skills() {
+  const { theme } = useTheme();
   const { ref } = useSectionInView({ sectionName: "Skills" });
 
   return (
@@ -33,8 +36,13 @@ export default function Skills() {
       <ul className="flex flex-wrap justify-center gap-2 text-lg !text-gray-800">
         {skillsData.map((skill, index) => (
           <motion.li
-            className="bg-white shadow-lg  borderBlack rounded-xl px-5 py-3
-            dark:bg-white/10 !dark:text-white/80 hover:bg-black hover:text-white transition-colors delay-150"
+          className={clsx(
+            "bg-white shadow-lg  borderBlack rounded-xl px-5 py-3 dark:bg-white/10 !dark:text-white/80  transition-colors delay-80",
+            {
+                "hover:bg-black hover:text-white":theme=="light"
+            }
+          )}
+
             key={index}
             variants={fadeInAnimationVariants}
             initial="initial"
